ae794af6f6d24fbf0a0619c9a1f99fd64cdd4124
darryldjohnson <darryl.johnson.w@gmail.com>
2021-04-10 03:12:35 -0500

    Web changes

--- lib/services/http_service.dart
+++ lib/services/http_service.dart
@@ -1,24 +1,15 @@
 import 'dart:async';
 import 'dart:convert';
 
-import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:http/http.dart' as http;
 
 Future<Map> httpRequest(String request) async {
   final completer = new Completer<Map>();
 
-  Map<String, String> requestHeaders = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Credentials": "true",
-    "Access-Control-Allow-Headers":
-    "Origin,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,locale",
-    "Access-Control-Allow-Methods": "GET, POST, OPTIONS"
-  };
-
   try {
     final uri = Uri.parse("$request");
 
-    final response = await http.get(uri, headers: requestHeaders);
+    final response = await http.get(uri);
 
     final result = json.decode(response.body);
 
